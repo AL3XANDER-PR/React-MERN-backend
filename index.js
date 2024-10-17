4b1213f2f8a67b37843ffa8044c5c5452b0276cb
AL3XANDER-PR <alexanderponte7@gmail.com>
2024-10-17 01:20:37 -0500

    Configuracion basica de Express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require("express");
 
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get("/", (req, res) => {
+  res.json({ ok: true });
+});
+
+// Escuchar peticiones
+app.listen(4000, () => {
+  console.log(`servidor en puerto ${4000}`);
+});
