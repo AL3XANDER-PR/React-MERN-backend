bc01378d6071c24632871bba00ca95034607f67a
AL3XANDER-PR <alexanderponte7@gmail.com>
2024-10-28 13:25:52 -0500

    Subida a Produccion

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const { dbConnection } = require("./database/config");
@@ -21,7 +22,10 @@ app.use(express.json());
 // Rutas
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
-// TODO: CRUD // Eventos
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
